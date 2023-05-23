bb1a15332bebe3c7e8e385eaac7e39e18cb483b6
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-05-23 00:12:39 -0400

    event handle lab complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function enterPassword(e) {
+    console.log(`Entering password...`);
+  }
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={enterPassword}
+        placeholder="Enter Password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
